8e1a2acef822bb39c2422e5d3ffc311a54cb27d0
maylakirana2605-ui <maylakirana2605@gmail.com>
2025-10-16 09:00:52 +0700

    While Menentukan nilai mahasiswa

---
+++ SiakadWhile20.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class SiakadWhile20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int i = 0, jml, nilai;
+
+        System.out.println("Masukkan Jumlah Mahasiswa : ");
+        jml = sc.nextInt();
+
+        while (i < jml) {
+            System.out.print("Masukkan Nilai Mahasiswa ke-" + (i+1) + " : ");
+            nilai = sc.nextInt();
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Nilai Tidak Valid. Masukkan Lagi Nilai Yang Valid!");
+                continue;
+            }
+
+            if (nilai > 80 && nilai <=100) {
+                System.out.println("Nilai Mahasiswa ke" + (i+1) + "Adalah A");
+                System.out.println("Bagus, Pertahankan Nilainya!");
+            } else if (nilai > 73 && nilai <=80) {
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah B+");
+            } else if (nilai > 65 && nilai <=73) {
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah B");
+            } else if (nilai > 60 && nilai <=65) {
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah C+");
+            } else if (nilai > 50 && nilai <=60) {
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah C");
+            } else if (nilai > 39 && nilai <=50) {
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah D");
+            } else {
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah E");
+            }
+            i++;
+        }
+
+
+    }
+}
