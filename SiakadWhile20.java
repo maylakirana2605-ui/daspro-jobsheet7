c9477c77b1bbdb6d54bbca83294b307382b4d54b
maylakirana2605-ui <maylakirana2605@gmail.com>
2025-10-16 09:19:35 +0700

    modifikasi menambhkan pesan

--- SiakadWhile20.java
+++ SiakadWhile20.java
@@ -19,20 +19,19 @@ public class SiakadWhile20 {
             }
 
             if (nilai > 80 && nilai <=100) {
-                System.out.println("Nilai Mahasiswa ke" + (i+1) + "Adalah A");
-                System.out.println("Bagus, Pertahankan Nilainya!");
+                System.out.println("Nilai Mahasiswa ke" + (i+1) + " adalah A");
             } else if (nilai > 73 && nilai <=80) {
-                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah B+");
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +" adalah B+");
             } else if (nilai > 65 && nilai <=73) {
-                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah B");
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +" adalah B");
             } else if (nilai > 60 && nilai <=65) {
-                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah C+");
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +" adalah C+");
             } else if (nilai > 50 && nilai <=60) {
-                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah C");
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +" adalah C");
             } else if (nilai > 39 && nilai <=50) {
-                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah D");
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +" adalah D");
             } else {
-                System.out.println("Nilai Mahasiswa ke-" + (i+1) +"Adalah E");
+                System.out.println("Nilai Mahasiswa ke-" + (i+1) +" adalah E");
             }
             i++;
         }
